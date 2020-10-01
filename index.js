5a0a6de8db680f92faff766fd8878942690e629a
rahuljha661 <rahuljha12527@gmail.com>
2020-10-01 18:53:00 +0530

    add index.js and home.ejs

--- index.js
+++ index.js
@@ -1,19 +1,37 @@
 const express = require("express");
-const path=require('path');
+const path = require("path");
 const port = 8000;
 
 const app = express();
 
-app.set('view engine','ejs');
-app.set('views',path.join(__dirname,'views'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-
+var contactList = [
+  {
+    name: "rahul jha",
+    phone: "8750023232",
+  },
+  {
+    name: "Tony stark",
+    phone: "123464378",
+  },
+  {
+    name: "Startup",
+    phone: "877743743",
+  },
+];
 app.get("/", function (req, res) {
-    console.log(__dirname);
-  return res.render('home');
+  return res.render("home", {
+    title: "Contacts List ",
+    contact_list: contactList,
+  });
 });
 
-app.listen(port, function (err) {   
+app.get("/practice", function (req, res) {
+  return res.render("practice", { title: "I am here to play" });
+});
+app.listen(port, function (err) {
   if (err) {
     console.log(`err in runnig the server ${err}`);
   }
