2b10c6e4bc43ac165e6a2e99495fd6ca82d8afa9
rahuljha661 <rahuljha12527@gmail.com>
2020-10-01 17:02:33 +0530

    commit file

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
-const express=require('express');
-const port=8000;
+const express = require("express");
+const path=require('path');
+const port = 8000;
 
-const app=express();
+const app = express();
+
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'views'));
 
 
-app.listen(port,function(err){
-    if(err){
-        console.log(`err in runnig the server ${err}`)
+app.get("/", function (req, res) {
+    console.log(__dirname);
+  return res.render('home');
+});
 
-    }
+app.listen(port, function (err) {   
+  if (err) {
+    console.log(`err in runnig the server ${err}`);
+  }
 
-    console.log(`Yup my Express  Server is running on Port:`,port);
-})
-
+  console.log(`Yup my Express  Server is running on Port:`, port);
+});
